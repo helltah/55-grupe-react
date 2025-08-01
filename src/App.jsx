f1b523c71c753054969017a06dd8714b4faa7b09
Vitalij <203612403+helltah@users.noreply.github.com>
2025-08-01 11:25:08 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
-
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Title } from "./components/Title";
+import { Calc } from "./components/Calc";
 export function App() {
   return (
     <>
-      CONTENT KEBABAS
+      <Header />
+      <Main>
+        <Title text="Labas rytas, Lietuva!" />
+        <div>🟨🟩🟥</div>
+        
+        <Calc a={7} b={5} />
+
+        <Title text="Apie mus" desc="Trumpas tinklapio pristatymas" />
+        <Title text="Paslaugos" desc="Musu teikiamu paslaugu aprasymas" />
+        <Title text="Kontaktai" desc="Zinau kur tu gyveni, todel geriau net nekotaktuok" />
+      </Main>
+      <Footer />
     </>
-  )
+  );
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export function Main({ children }) {
+
+    return (
+        <main> 
+            MAIN CONTENT - START
+            {children}
+            MAIN CONTENT - END
+        </main>
+      )
+}
